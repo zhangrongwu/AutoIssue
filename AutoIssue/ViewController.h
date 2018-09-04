d216ed0665854b99f2b64ba51b56bb28e9a23369
zhangrongwu <zhangrongwuios@icloud.com>
2018-09-04 13:42:58 +0800

    Initial Commit

---
+++ AutoIssue/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AutoIssue
+//
+//  Created by zhangrongwu on 2018/9/4.
+//  Copyright © 2018年 ENN. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ AutoIssueTests/AutoIssueTests.m
@@ -0,0 +1,39 @@
+//
+//  AutoIssueTests.m
+//  AutoIssueTests
+//
+//  Created by zhangrongwu on 2018/9/4.
+//  Copyright © 2018年 ENN. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface AutoIssueTests : XCTestCase
+
+@end
+
+@implementation AutoIssueTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
